3249db21a46cf8537b50f42055bbd3945f570ab2
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2024-01-07 22:50:07 +0100

    Create library, refactor

---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+use std::collections::BTreeMap;
+use std::io::{BufRead, Read};
+
+type SecretId = String;
+
+type SecretData = String;
+
+pub struct Secrets(BTreeMap<SecretId, SecretData>);
+
+impl Secrets {
+    pub fn new(items: BTreeMap<SecretId, SecretData>) -> Self {
+        Self(items)
+    }
+
+    pub fn get(&self, id: &str) -> Option<&SecretData> {
+        self.0.get(id)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = (&SecretId, &SecretData)> {
+        self.0.iter()
+    }
+}
+
+impl<T> From<T> for Secrets
+where
+    T: Read + BufRead,
+{
+    fn from(reader: T) -> Self {
+        let mut lines = reader.lines();
+
+        // First line is the password
+        let password = lines.next().map(|line| line.unwrap());
+
+        // All other lines are key-value pairs, separated by a colon
+        let mut other: BTreeMap<SecretId, SecretData> = lines
+            .filter_map(|line| {
+                let line = line.unwrap();
+                let mut parts = line.splitn(2, ':');
+                let id = parts.next()?;
+                let data = parts.next()?.trim_start();
+                Some((id.to_string(), data.to_string()))
+            })
+            .collect();
+
+        // Add the password, only if it is non-empty
+        if let Some(password) = password {
+            other.insert("password".to_string(), password);
+        }
+
+        Self(other)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_password_store_single_line() {
+        let input = b"THIS_IS_THE_PASSWORD".as_slice();
+        let secrets = Secrets::from(input);
+        assert_eq!(secrets.get("password").unwrap(), "THIS_IS_THE_PASSWORD");
+    }
+
+    #[test]
+    fn test_password_store_multi_line() {
+        let input = b"THIS_IS_THE_PASSWORD\nusername: test".as_slice();
+        let secrets = Secrets::from(input);
+        assert_eq!(secrets.get("password").unwrap(), "THIS_IS_THE_PASSWORD");
+        assert_eq!(secrets.get("username").unwrap(), "test");
+    }
+
+    #[test]
+    fn test_invalid_multiline() {
+        let input = b"THIS_IS_THE_PASSWORD\nasdasd\nqweqwe".as_slice();
+        let secrets = Secrets::from(input);
+        assert_eq!(secrets.get("password").unwrap(), "THIS_IS_THE_PASSWORD");
+    }
+
+    #[test]
+    fn test_empty_input() {
+        let input = b"".as_slice();
+        let secrets = Secrets::from(input);
+        assert!(secrets.get("password").is_none());
+        assert!(secrets.0.is_empty());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::collections::BTreeMap;
-use std::io::{BufRead, Read};
+use passmumbler::Secrets;
 
 use gtk4 as gtk;
 
@@ -59,7 +59,7 @@ fn build_ui(application: &gtk::Application) {
         .valign(gtk::Align::Center)
         .build();
 
-    for (label, data) in secrets.0.iter() {
+    for (label, data) in secrets.iter() {
         let btn = gtk::Button::with_label(label);
         btn.connect_clicked(clone!(@to-owned data, @weak clipboard => move |_btn| {
             clipboard.set_text(data.as_str());
@@ -86,48 +86,6 @@ struct Cli {
     stdin: bool,
 }
 
-type SecretId = String;
-
-type SecretData = String;
-
-struct Secrets(BTreeMap<SecretId, SecretData>);
-
-impl Secrets {
-    fn get(&self, id: &str) -> Option<&SecretData> {
-        self.0.get(id)
-    }
-}
-
-impl<T> From<T> for Secrets
-where
-    T: Read + BufRead,
-{
-    fn from(reader: T) -> Self {
-        let mut lines = reader.lines();
-
-        // First line is the password
-        let password = lines.next().map(|line| line.unwrap());
-
-        // All other lines are key-value pairs, separated by a colon
-        let mut other: BTreeMap<SecretId, SecretData> = lines
-            .filter_map(|line| {
-                let line = line.unwrap();
-                let mut parts = line.splitn(2, ':');
-                let id = parts.next()?;
-                let data = parts.next()?.trim_start();
-                Some((id.to_string(), data.to_string()))
-            })
-            .collect();
-
-        // Add the password, only if it is non-empty
-        if let Some(password) = password {
-            other.insert("password".to_string(), password);
-        }
-
-        Self(other)
-    }
-}
-
 impl From<Cli> for Secrets {
     fn from(value: Cli) -> Self {
         let mut inner = BTreeMap::new();
@@ -140,41 +98,6 @@ impl From<Cli> for Secrets {
             inner.insert("password".to_string(), password);
         }
 
-        Self(inner)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_password_store_single_line() {
-        let input = b"THIS_IS_THE_PASSWORD".as_slice();
-        let secrets = Secrets::from(input);
-        assert_eq!(secrets.get("password").unwrap(), "THIS_IS_THE_PASSWORD");
-    }
-
-    #[test]
-    fn test_password_store_multi_line() {
-        let input = b"THIS_IS_THE_PASSWORD\nusername: test".as_slice();
-        let secrets = Secrets::from(input);
-        assert_eq!(secrets.get("password").unwrap(), "THIS_IS_THE_PASSWORD");
-        assert_eq!(secrets.get("username").unwrap(), "test");
-    }
-
-    #[test]
-    fn test_invalid_multiline() {
-        let input = b"THIS_IS_THE_PASSWORD\nasdasd\nqweqwe".as_slice();
-        let secrets = Secrets::from(input);
-        assert_eq!(secrets.get("password").unwrap(), "THIS_IS_THE_PASSWORD");
-    }
-
-    #[test]
-    fn test_empty_input() {
-        let input = b"".as_slice();
-        let secrets = Secrets::from(input);
-        assert!(secrets.get("password").is_none());
-        assert!(secrets.0.is_empty());
+        Self::new(inner)
     }
 }
